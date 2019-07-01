7fdb38428ac54b7c9dcb0c61d7eeac5b86cca6f0
TahaSalva444 <taha_salva@hotmail.com>
2019-07-01 22:33:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navigationdemo/AccountFragment.kt
@@ -0,0 +1,34 @@
+package com.example.navigationdemo
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_account.*
+
+
+class AccountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nextBtn.setOnClickListener {
+            val nameBundle = Bundle()
+            nameBundle.putString("nameArg",nameText.text.toString())
+            it.findNavController().navigate(R.id.action_accountFragment_to_nameFragment2,nameBundle)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/navigationdemo/mainFragment.kt
@@ -0,0 +1,32 @@
+package com.example.navigationdemo
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+class mainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        goToAccountBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.to_accountFragment))
+        goToSettingBtn.setOnClickListener { it.findNavController().navigate(R.id.to_settingsFragment) }
+    }
+
+
+}
